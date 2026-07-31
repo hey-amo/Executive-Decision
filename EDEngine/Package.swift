b8fd1dfb4d2b754a9779780e54771e36554c9b75
AD <silentflute.ad@gmail.com>
2026-07-31 07:38:59 +0100

    Clean out the project and make a package to handle the game engine, rules first

---
+++ EDEngine/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EDEngine",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "EDEngine",
+            targets: ["EDEngine"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "EDEngine"),
+        .testTarget(
+            name: "EDEngineTests",
+            dependencies: ["EDEngine"]
+        ),
+    ]
+)
